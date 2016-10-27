2cfea19f6233b326f1ee53366a2670df0f485b6c
sc0ttruss <scott.russell@inetmedia.co.uk>
2016-10-27 18:10:43 +0300

    rename keys to delivery.pem and builder_key as the build node install process requires these names

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,12 +20,16 @@ default['automate']['url']['delivery'] = 'automate.myorg.chefdemo.net'
 # need chefdk, for noww only one target platform ( RHEL 7.2 )
 # but will add others over time,  Used by install-build-node command
 default['automate']['chefdk']['redhat'] = 'chefdk-0.19.6-1.el7.x86_64.rpm'
+# Need chef supermarket url
+default['automate']['supermarket_server_fqdn'] = 'https://supermarket.myorg.chefdemo.net'
+
 # Parameters for build node(s)
 default['automate']['build_node']['fqdn'] = 'builder1.myorg.chefdemo.net'
 default['automate']['build_node']['username'] = 'vagrant'
 default['automate']['build_node']['password'] = 'vagrant'
-default['automate']['build_node']['installer'] = "/opt/delviery/#{node['automate']['chefdk']['redhat']}"
+default['automate']['build_node']['installer'] = "/opt/delivery/#{node['automate']['chefdk']['redhat']}"
+# not implemented in this script but you can use it
+# need to add the ''--ssh_identity_file' switch to the
+# 'delivery-ctl install-build-node' command
 default['automate']['build_node']['ssh_identity_file'] = ''
 default['automate']['build_node']['port'] = '22'
-
---fqdn --username --password --installer --ssh-identity-file --port
--- recipes/installbuildnode.rb
+++ recipes/installbuildnode.rb
@@ -9,12 +9,58 @@ remote_file "/opt/delivery/#{node['automate']['chefdk']['redhat']}" do
   # checksum 'abc123'
 end
 
-# install build node
+# install supermarket certificate
 
-bash 'reconfigure delivery' do
+bash 'fetch supermarket certificates' do
   user 'root'
   cwd '/var/tmp'
   code <<-EOH
-  delivery-ctl install-build-node --fqdn "#{node['automate']['build_node']['fqdn']}" --username "#{node['automate']['build_node']['username']}" --password "#{node['automate']['build_node']['password']}" --installer "#{node['automate']['build_node']['installer']}" --ssh-identity-file "#{node['automate']['build_node']['ssh_identity_file']}" --port "#{node['automate']['build_node']['port']}"
+  knife ssl fetch #{node['automate']['supermarket_server_fqdn']}
+  EOH
+end
+
+# copy supermarket to correct location, where automate expects it
+
+bash 'copy supermarket certificate' do
+  user 'root'
+  cwd '/var/tmp'
+  code <<-EOH
+  cp /root/.chef/trusted_certs/*.crt /opt/delivery
+  EOH
+end
+
+# Copy the myorg_sshkey to builder_ssh.  Need this needs some work
+# as it is already on the share from the default recipe
+# seems that it is hard coded as "builder_key" for the  install-build-node
+
+remote_file "/etc/delivery/builder_key" do
+  source "file:///etc/delivery/#{node['automate']['organisation']}_ssh_key"
+  owner 'root'
+  group 'root'
+  mode 00755
+  # checksum 'abc123'
+end
+
+# Copy the srv-delviery.pen key to builder_ssh.  Need this needs some work
+# as it is already on the share from the default recipe
+# seems that it is hard coded as "delivery.pem" for the  install-build-node
+
+
+remote_file "/etc/delivery/delivery.pem" do
+  source "file:///etc/delivery/#{node['automate']['chef_user_private_key']}"
+  owner 'root'
+  group 'root'
+  mode 00755
+  # checksum 'abc123'
+end
+
+
+# install build node
+
+bash 'install build node' do
+  user 'root'
+  cwd '/var/tmp'
+  code <<-EOH
+  delivery-ctl install-build-node --fqdn "#{node['automate']['build_node']['fqdn']}" --username "#{node['automate']['build_node']['username']}" --password "#{node['automate']['build_node']['password']}" --installer "#{node['automate']['build_node']['installer']}" --port "#{node['automate']['build_node']['port']}"
   EOH
 end
